60d2135f650abe0668610b59fa2382d30d3e1218
ypan69 <ypan69@myseneca.ca>
2026-07-17 13:59:56 -0400

    Add files via upload

--- project/prod/alb/main.tf
+++ project/prod/alb/main.tf
@@ -7,9 +7,9 @@ provider "aws" {
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
-    bucket = "${var.env}-acs730-${lower(local.prefix)}-${lower(local.owner)}"
-    key    = "${var.env}-network/terraform.tfstate"
-    region = "us-east-1"
+    bucket = "${var.env}-${lower(local.prefix)}-${lower(local.owner)}"       // Bucket from where to GET Terraform State
+    key    = "${var.env}-networkterraform.tfstate" // Object name in the bucket to GET Terraform State
+    region = "us-east-1" // Region where bucket created
   }
 }
 
@@ -17,9 +17,9 @@ data "terraform_remote_state" "network" {
 data "terraform_remote_state" "webservers" {
   backend = "s3"
   config = {
-    bucket = "${var.env}-acs730-${lower(local.prefix)}-${lower(local.owner)}"
-    key    = "${var.env}-webservers/terraform.tfstate"
-    region = "us-east-1"
+    bucket = "${var.env}-${lower(local.prefix)}-${lower(local.owner)}"       // Bucket from where to GET Terraform State
+    key    = "${var.env}-webserversterraform.tfstate" // Object name in the bucket to GET Terraform State
+    region = "us-east-1" // Region where bucket created
   }
 }
 
